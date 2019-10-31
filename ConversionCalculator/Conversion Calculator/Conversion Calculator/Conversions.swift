677ff1226fe50e4284a90db416fd1cc3a0dc98f9
ChaseMiles <42549771+ChaseMiles@users.noreply.github.com>
2019-10-31 15:34:00 -0500

    Add files via upload

---
+++ ConversionCalculator/Conversion Calculator/Conversion Calculator/Conversions.swift
@@ -0,0 +1,15 @@
+//
+//  Conversions.swift
+//  Conversion Calculator
+//
+//  Created by Chase Miles on 10/31/19.
+//  Copyright © 2019 Chase Miles. All rights reserved.
+//
+
+import Foundation
+
+struct Conversions {
+    var label: String
+    var inputUnit: String
+    var outputUnit: String
+}
